8e9eb37ed61dc1aaee56f03a728cea38e2ddedf8
Riku Virtanen <virtanen.riku91@gmail.com>
2021-10-27 17:52:16 +0300

    implemented WebSecurityServiceImpl, UserController and UserRepository

---
+++ src/main/java/hh/swd20/discgolfbag/web/UserDetailsServiceImpl.java
@@ -0,0 +1,32 @@
+package hh.swd20.discgolfbag.web;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.authority.AuthorityUtils;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import hh.swd20.discgolfbag.domain.User;
+import hh.swd20.discgolfbag.domain.UserRepository;
+
+@Service
+public class UserDetailsServiceImpl implements UserDetailsService {
+	
+	private final UserRepository repository;
+	
+	@Autowired
+	public UserDetailsServiceImpl(UserRepository userRepository) {
+		this.repository = userRepository; 
+	}
+	
+	@Override
+	public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+		User curruser = repository.findByUsername(username);
+		UserDetails user = new org.springframework.security.core.userdetails.User(
+				username, 
+				curruser.getPasswordHash(), 
+				AuthorityUtils.createAuthorityList(curruser.getRole()));
+		return user;
+	}
+}
