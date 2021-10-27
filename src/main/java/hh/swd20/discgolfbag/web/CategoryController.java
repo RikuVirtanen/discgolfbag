b545e9c73bbd695bce2c998d79adbb3d5001ea53
Riku Virtanen <virtanen.riku91@gmail.com>
2021-10-27 16:53:47 +0300

    implemented categoryController

--- src/main/java/hh/swd20/discgolfbag/web/CategoryController.java
+++ src/main/java/hh/swd20/discgolfbag/web/CategoryController.java
@@ -1,5 +1,35 @@
 package hh.swd20.discgolfbag.web;
 
-public class CategoryController {
+import java.util.List;
+import java.util.Optional;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import hh.swd20.discgolfbag.domain.Category;
+import hh.swd20.discgolfbag.domain.CategoryRepository;
+
+@CrossOrigin
+@Controller
+public class CategoryController {
+	
+	@Autowired
+	private CategoryRepository repository;
+	
+	/*********************** RESTFUL SERVICES *********************************/
+	
+	@RequestMapping(value = "/categories", method = RequestMethod.GET)
+	public @ResponseBody List<Category> getCategoriesRest() {
+		return (List<Category>) repository.findAll();
+	}
+	
+	@RequestMapping(value = "/categories/{id}", method = RequestMethod.GET)
+	public @ResponseBody Optional<Category> findCategoryRest(@PathVariable("id") Long categoryId) {
+		return repository.findById(categoryId);
+	}
 }
