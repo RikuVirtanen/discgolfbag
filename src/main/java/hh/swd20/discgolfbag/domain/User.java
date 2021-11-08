9757e6b61a24cbaecaca9ff34e99289e83accf77
Riku Virtanen <virtanen.riku91@gmail.com>
2021-11-08 11:13:22 +0200

    updated database to postgresql

--- src/main/java/hh/swd20/discgolfbag/domain/User.java
+++ src/main/java/hh/swd20/discgolfbag/domain/User.java
@@ -8,10 +8,12 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.OneToOne;
+import javax.persistence.Table;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 
 @Entity
+@Table(name="usertable")
 public class User {
 	
 	@Id
