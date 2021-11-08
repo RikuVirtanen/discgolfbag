c6030023897628cdcc97250b0c1ffd79995ffc6d
Riku Virtanen <virtanen.riku91@gmail.com>
2021-11-08 13:28:56 +0200

    bug fixes

--- src/main/java/hh/swd20/discgolfbag/DiscgolfbagApplication.java
+++ src/main/java/hh/swd20/discgolfbag/DiscgolfbagApplication.java
@@ -1,27 +1,11 @@
 package hh.swd20.discgolfbag;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
-
-import hh.swd20.discgolfbag.domain.BagRepository;
-import hh.swd20.discgolfbag.domain.Category;
-import hh.swd20.discgolfbag.domain.CategoryRepository;
-import hh.swd20.discgolfbag.domain.Company;
-import hh.swd20.discgolfbag.domain.CompanyRepository;
-import hh.swd20.discgolfbag.domain.Disc;
-import hh.swd20.discgolfbag.domain.DiscRepository;
-import hh.swd20.discgolfbag.domain.Plastic;
-import hh.swd20.discgolfbag.domain.PlasticRepository;
-import hh.swd20.discgolfbag.domain.User;
-import hh.swd20.discgolfbag.domain.UserRepository;
 
 @SpringBootApplication
 public class DiscgolfbagApplication {
-	private static final Logger log = LoggerFactory.getLogger(DiscgolfbagApplication.class);
+	//private static final Logger log = LoggerFactory.getLogger(DiscgolfbagApplication.class);
 
 	public static void main(String[] args) {
 		SpringApplication.run(DiscgolfbagApplication.class, args);
