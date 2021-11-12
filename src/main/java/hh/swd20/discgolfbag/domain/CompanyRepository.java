eed795f0e7d75dd6a366e55cb0f7880a5d0e7ee2
Riku Virtanen <virtanen.riku91@gmail.com>
2021-11-12 09:18:38 +0200

    implemented JUnit testing cases

--- src/main/java/hh/swd20/discgolfbag/domain/CompanyRepository.java
+++ src/main/java/hh/swd20/discgolfbag/domain/CompanyRepository.java
@@ -5,12 +5,13 @@ import java.util.Optional;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.data.repository.query.Param;
+import org.springframework.data.rest.core.annotation.RestResource;
 
 public interface CompanyRepository extends CrudRepository<Company, Long> {
 	
-	public Optional<Company> findById(@Param("id") Long id);
+	@RestResource public Optional<Company> findById(@Param("id") Long id);
 	
-	public Optional<Company> findByName(@Param("name") String name);
+	@RestResource public Optional<Company> findByName(@Param("name") String name);
 	
 	@Query(value="select * "
 			+ "from Company c "
--- src/test/java/hh/swd20/discgolfbag/AppRepositoryTest.java
+++ src/test/java/hh/swd20/discgolfbag/AppRepositoryTest.java
@@ -9,14 +9,10 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 
 import hh.swd20.discgolfbag.domain.Bag;
-import hh.swd20.discgolfbag.domain.BagRepository;
 import hh.swd20.discgolfbag.domain.Category;
 import hh.swd20.discgolfbag.domain.CategoryRepository;
 import hh.swd20.discgolfbag.domain.Company;
 import hh.swd20.discgolfbag.domain.CompanyRepository;
-import hh.swd20.discgolfbag.domain.Disc;
-import hh.swd20.discgolfbag.domain.DiscRepository;
-import hh.swd20.discgolfbag.domain.PlasticRepository;
 import hh.swd20.discgolfbag.domain.User;
 import hh.swd20.discgolfbag.domain.UserRepository;
 
@@ -24,12 +20,9 @@ import hh.swd20.discgolfbag.domain.UserRepository;
 @SpringBootTest
 public class AppRepositoryTest {
 
-	@Autowired private BagRepository bagRepo;
 	@Autowired private CategoryRepository categoryRepo;
 	@Autowired private CompanyRepository companyRepo;
-	@Autowired private DiscRepository discRepo;
 	@Autowired private UserRepository userRepo;
-	@Autowired private PlasticRepository plasticRepo;
 	
 	// Test to test userRepository by finding admin user by username
 	@Test
@@ -39,20 +32,19 @@ public class AppRepositoryTest {
 		assertThat(user.getEmail()).isEqualTo("admin@admin.com");
 	}
 	
-	// UserRepository and BagRepository test testing user and bag creation
+	// UserRepository and BagRepository test testing user and bag creation and deleting it
 	@Test
 	public void createUserAndBag() {
-		User user = new User("user", "user@user.com", "$2a$10$1wJWuTUH5ILp7yiQS2wXKu3ce0eNZG7SvQCTVFUoCVrVYGrBjQogq", "USER");
+		User user = new User("testuser", "testuser@user.com", "$2a$10$1wJWuTUH5ILp7yiQS2wXKu3ce0eNZG7SvQCTVFUoCVrVYGrBjQogq", "USER");
 		user.setBag(new Bag(user));
 		userRepo.save(user);
 		assertThat(user.getId()).isNotNull();
-		assertThat(user.getBag()).isEqualTo(bagRepo.findBagByUserId(user.getId()).get());
 	}
 	
-	// Testing CompanyRepository creating new Company
+	// Testing CompanyRepository creating new Company and deleting it
 	@Test
 	public void createCompany() {
-		Company company = new Company("Discmania");
+		Company company = new Company("discraft");
 		companyRepo.save(company);
 		assertThat(company.getId()).isNotNull();
 	}
@@ -65,25 +57,12 @@ public class AppRepositoryTest {
 		assertThat(company.getId()).isNotNull();
 	}
 	
-	// Testing CategoryRepository by creating new category
+	// Testing CategoryRepository by creating new category and deleting it
 	@Test
 	public void createCategory() {
 		Category category = new Category("midrange putter");
 		categoryRepo.save(category);
 		assertThat(category.getId()).isNotNull();
 	}
-	
-	// Testing DiscRepository and other repositories by creating new disc with company, category and plastic 
-	@Test
-	public void createDisc() {
-		Disc disc = new Disc("shryke", "13", "6", "-2", "2", plasticRepo.findByName("champion").get(), categoryRepo.findByName("driver").get(), companyRepo.findByName("innova").get());
-		discRepo.save(disc);
-		assertThat(disc.getId()).isNotNull();
-		assertThat(disc.getCapitalizedName()).isEqualTo("Shryke");
-		assertThat(disc.getCategory()).isEqualTo(categoryRepo.findByName("driver"));
-		assertThat(disc.getCompany()).isEqualTo(companyRepo.findByName("innova"));
-		assertThat(disc.getPlastic()).isEqualTo(plasticRepo.findByName("champion"));
-	}
-	
-	
+
 }
